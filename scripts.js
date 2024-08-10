b15a7912fb7cc18389f4b52b1e581f401921145f
Amir Mohammad Kharazi <a.m.kharazi.ac@gmail.com>
2024-08-10 15:32:03 +0330

    updated

---
+++ scripts.js
@@ -0,0 +1,66 @@
+// Make the DIV element draggable:
+// dragElement(document.getElementById("draggable-icon1"));
+// dragElement(document.getElementById("draggable-icon2"));
+// dragElement(document.getElementById("draggable-icon3"));
+// dragElement(document.getElementById("draggable-icon4"));
+// dragElement(document.getElementById("draggable-icon5"));
+// dragElement(document.getElementById("draggable-icon6"));
+// dragElement(document.getElementById("draggable-icon7"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
+function updateTime() {
+  const timeOptionDiv = document.getElementById('time-options');
+  const now = new Date();
+  let hours = now.getHours();
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12;
+  hours = hours ? hours : 12; // Convert hour '0' to '12'
+  const timeString = `${hours}:${minutes} ${ampm}`;
+  timeOptionDiv.textContent = timeString;
+}
+
+updateTime(); // Initial call to display the time immediately
+setInterval(updateTime, 1000);
