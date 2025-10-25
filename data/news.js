7d908b56f716950413577d856e0292cb40aceca3
Amir Mohammad Kharazi <a.m.kharazi.ac@gmail.com>
2025-10-25 10:12:59 +0330

    updated

--- data/news.js
+++ data/news.js
@@ -1,6 +1,16 @@
 const newsData = {
     news: [
          {
+            title: "The Chronicle of ALGA Is Set Forth to the High Council of IEEE",
+            date: "2025-10-25",
+            image: "https://d197for5662m48.cloudfront.net/images/institution/26407/whitelabel_logo_image/5cfcfb7bb5078907655a652ac0444b47.png",
+            url: "https://www.techrxiv.org/doi/full/10.36227/techrxiv.176108421.18380360/v1",
+            description: "At long last, the grand codex *ALGA* — forged through countless nights of study and experimentation — has been dispatched to the venerable halls of the **IEEE Transactions on Pattern Analysis and Machine Intelligence**. There, among the scrolls of the learned, it shall face the scrutiny of the Council of Reviewers. Until their verdict is revealed, an archived version rests safely within the TechRxiv vaults, accessible to all seekers of knowledge through its sacred DOI: [10.36227/techrxiv.176108421.18380360/v1].",
+            category: "Announcement",
+            showLatestImage: true,
+            showImage: true
+        },
+         {
             title: "The Great CompNeuro Quest Comes to a Close",
             date: "2025-07-25",
             image: "https://github.com/NeuromatchAcademy/course-content/blob/main/tutorials/static/ConceptMap.png?raw=1",
